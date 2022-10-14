0fb7d774284a5cfec29c1b59be76c85c3374a263
Hoki wahyuono pranto <hoki.ikki@gmail.com>
2022-10-14 12:30:25 +0700

    day-5

--- day-5/task/blog.js
+++ day-5/task/blog.js
@@ -3,12 +3,12 @@ let dataBlog = []
 function addBlog(event) {
     event.preventDefault()
 
-    let tittle = document.getElementById("tittle").value
+    let tittle = document.getElementById("input-tittle").value
     let start = document.getElementById("start").value
     let end = document.getElementById("end").value
-    let description = document.getElementById("input-content").value
+    let content = document.getElementById("input-content").value
     let tech = document.getElementById("js").checked = true;
-    let image = document.getElementById("input-image").files[0]
+    let image = document.getElementById("input-blog-image").files[0]
 
     // buat url gambar nantinya tampil
     image = URL.createObjectURL(image)
@@ -18,7 +18,7 @@ function addBlog(event) {
         tittle,
         start,
         end,
-        description,
+        content,
         tech,
         image,
         postAt: new Date(),
@@ -43,54 +43,103 @@ function renderBlog() {
                 <img src="${dataBlog[index].image}">
             </div>
             <div class="blog-content">
-            <div class="btn-group">
-            <button class="btn-edit">Edit</button>
-            <button class="btn-post">Delete</button>
+                <div class="btn-group">
+                    <button class="btn-edit">Edit</button>
+                    <button class="btn-post">Delete</button>
                 </div>
                 <h1>
                     <a href="blog-detail.html" target="_blank">
-                        ${dataBlog[index].title}
+                        ${dataBlog[index].tittle}
                     </a>
                 </h1>
                 <div class="detail-blog-content">
+                    ${getFullTime(dataBlog[index].postAt)} | ${dataBlog[index].author}
                 </div>
                 <p>
                     ${dataBlog[index].content}
                 </p>
+                <div>
+                    <p style="font-size: 15px; color: grey">${getDistanceTime(dataBlog[index].postAt)}</p>
+                </div>
             </div>
         </div>
         `
     }
 }
 
+
 function getFullTime(time) {
-    //time = new Date ()
-    //cosnsole.log(time)
+    // time = new Date()
+    // console.log(time)
 
-    let monthName = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Agt','Sep','Oct','Nov','Dec']
-    //console.log(monthName[9]);
+    let monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    // console.log(monthName[9])
 
+    // 14
     let date = time.getDate()
-    console.log(date);
+    console.log(date)
 
+    // 9
     let monthIndex = time.getMonth()
-    console.log(monthIndex);
+    console.log(monthIndex)
 
-    let year = time.getFullYear
-    console.log(year);
+    // 2022
+    let year = time.getFullYear()
+    console.log(year)
 
     let hours = time.getHours()
     let minutes = time.getMinutes()
 
+    console.log(hours)
+
     if (hours <= 9) {
-        hours = "0" = hours
-        
-    } else if (minutes <= 9) {
-    minutes = "0" + minutes
+        hours = "0" + hours
+    } 
     
+    if (minutes <= 9) {
+        minutes = "0" + minutes
     }
 
-    // 
-    return `$(date) $(monthName[monthIndex]) $(year) $(hours):$(minutes)` WIB
-
+    // 14 Oct 2022 09:07 WIB
+    return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`
 }
+
+function getDistanceTime(time) {
+    let timeNow = new Date()
+    let timePost = time
+
+    let distance = timeNow - timePost //milisecond
+    console.log(distance)
+
+    let milisecond = 1000 // milisecond
+    let secondInHours = 3600 // 1 jam = 3600 detik
+    let hoursInDay = 24 // 1 hari = 24 jam
+
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+    let distanceMinutes = Math.floor(distance / (milisecond * 60))
+    let distanceSecond = Math.floor(distance / milisecond)
+
+    if (distanceDay > 0) {
+        return `${distanceDay} day(s) ago`
+    } else if (distanceHours > 0) {
+        return `${distanceHours} hour(s) ago`
+    } else if (distanceMinutes > 0) {
+        return `${distanceMinutes} minute(s) ago`
+    } else {
+        return `${distanceSecond} second(s) ago`
+    }
+}
+
+// 1#
+
+/*setInterval(function() {
+    renderBlog()
+}, 3000)
+
+// 2#
+// setInterval(intervalFunction, 3000)
+
+// function intervalFunction() {
+//     renderBlog()
+ }*/
